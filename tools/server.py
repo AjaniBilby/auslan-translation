c68ce0dfc26b82e1afcf626d3e5ef5506e91831c
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-09-15 15:51:01 +1000

    ~cleaned up

--- tools/server.py
+++ tools/server.py
@@ -5,7 +5,7 @@ import torch
 from transformers import BartForConditionalGeneration, BartTokenizer
 from flask import Flask, request, jsonify
 
-model_path = '../saved_models/epoch_36'
+model_path = './saved_models/epoch_36'
 
 
 class SimpleBART(torch.nn.Module):
